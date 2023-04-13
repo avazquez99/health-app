131d625e1a321fea6a39ede37e574bfe0748bc9f
avazquez99 <vazqueza193@gmail.com>
2023-04-14 00:13:45 +0100

    Creacion de proyecto con Spring

---
+++ health-service-app/src/main/java/com/example/healthserviceapp/HealthServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceAppApplication.class, args);
+	}
+
+}
---
+++ health-service-app/src/test/java/com/example/healthserviceapp/HealthServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

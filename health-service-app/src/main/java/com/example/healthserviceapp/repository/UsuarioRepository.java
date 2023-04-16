b98cd9444534451f15cc8116a55c1bfad8d52f0f
Alexandro Moroz <AlexandroMoroz5@gmail.com>
2023-04-16 17:16:41 -0300

    creación de Query que busca un usuario por email

--- health-service-app/src/main/java/com/example/healthserviceapp/repository/UsuarioRepository.java
+++ health-service-app/src/main/java/com/example/healthserviceapp/repository/UsuarioRepository.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Repository;
 import com.example.healthserviceapp.entity.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String>{
 
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    public Usuario buscarPorEmail(@Param("email") String email);
+    
 }

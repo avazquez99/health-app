7946fe1addcd757012a44080f2115929695ef762
BEMP1 <brunoemoyano@gmail.com>
2023-04-16 17:14:21 -0300

    index, carpeta imagenes, js y carpteta css

---
+++ health-service-app/src/main/resources/js/slider.js
@@ -0,0 +1,32 @@
+(function(){
+  //Propiedades slider
+  var propSlider = {
+
+    slider: document.getElementById('slider'),
+    primerSlide: null
+
+  }
+
+  //Métodos slider
+  var metSlider = {
+
+    inicio: function(){
+      setInterval(metSlider.moverSlide, 3000);
+    },
+
+    moverSlide: function(){
+      propSlider.slider.style.transition = 'all 1s ease';
+      propSlider.slider.style.marginLeft = '-100%';
+
+      setTimeout(function(){
+        propSlider.primerSlide = propSlider.slider.firstElementChild;
+        propSlider.slider.appendChild(propSlider.primerSlide);
+        propSlider.slider.style.transition = 'unset';
+        propSlider.slider.style.marginLeft = 0;
+      }, 1000);
+
+    }
+  }
+
+  metSlider.inicio();
+}())

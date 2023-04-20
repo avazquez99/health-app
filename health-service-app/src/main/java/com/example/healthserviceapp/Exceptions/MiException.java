b3c5f71b87ea5a397d4c103f22b50fa3902935be
avazquez99 <vazqueza193@gmail.com>
2023-04-20 01:10:12 +0100

    Eliminación de líneas de texto

--- health-service-app/src/main/java/com/example/healthserviceapp/Exceptions/MiException.java
+++ health-service-app/src/main/java/com/example/healthserviceapp/Exceptions/MiException.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.healthserviceapp.Exceptions;
 
-/**
- *
- * @author ramir
- */
+
 public class MiException extends Exception {
     public MiException(String msg){
         super(msg);

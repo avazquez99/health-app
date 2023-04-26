465311d78b36d787a9a3a7ea9d786affab8e4617
Alexandro Moroz <AlexandroMoroz5@gmail.com>
2023-04-25 23:55:33 -0300

    cambio del formulario de persona para que pase de crearse una persona a crearse directamente un paciente

--- health-service-app/src/main/java/com/example/healthserviceapp/controllers/PersonaControlador.java
+++ health-service-app/src/main/java/com/example/healthserviceapp/controllers/PersonaControlador.java
@@ -1,13 +1,12 @@
 package com.example.healthserviceapp.controllers;
 
+import com.example.healthserviceapp.entity.Paciente;
 import com.example.healthserviceapp.entity.Persona;
 import com.example.healthserviceapp.entity.Usuario;
 import com.example.healthserviceapp.enums.ObraSocial;
 import com.example.healthserviceapp.enums.Sexo;
 import com.example.healthserviceapp.service.PersonaService;
 import com.example.healthserviceapp.service.UsuarioService;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -18,21 +17,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/perfil")
 public class PersonaControlador {
-
-    @Autowired
-    private PersonaService personaService;
-
-    @Autowired
-    private UsuarioService usuarioService;
-    
+   
     @GetMapping("/datos")
     @PreAuthorize("hasAnyRole('ROLE_PACIENTE', 'ROLE_PROFESIONAL')")
     public String vistaDatosPerfil(ModelMap modelo, HttpSession session) {
@@ -44,37 +34,13 @@ public class PersonaControlador {
         if (session.getAttribute("usuariosession") instanceof Usuario) {
             tipo = "Usuario";           
         }
-        if (session.getAttribute("usuariosession") instanceof Persona) {
-            tipo = "Persona";
-            Persona persona = (Persona) session.getAttribute("usuariosession");
-            modelo.put("persona", persona);
+        if (session.getAttribute("usuariosession") instanceof Paciente) {
+            tipo = "Paciente";
+            Paciente paciente = (Paciente) session.getAttribute("usuariosession");
+            modelo.put("paciente", paciente);
         }
         modelo.put("tipo", tipo);
         return "form.html";
-    }
-
-    @PostMapping("/actualizar")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_PACIENTE', 'ROLE_PROFESIONAL')")
-    public String actualizarDatos(@RequestParam String nombre, @RequestParam String apellido, MultipartFile imagen,
-            @RequestParam String domicilio, @RequestParam Integer dni, @RequestParam Sexo sexo,
-            @RequestParam String fechaNacimiento, HttpSession session) throws Exception {
-
-        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-
-        Date dataFormateada = formato.parse(fechaNacimiento);
-
-        if (session.getAttribute("usuariosession") instanceof Persona) {
-            Persona persona = (Persona) session.getAttribute("usuariosession");
-            personaService.modifyPersona(persona, nombre, apellido, sexo, dataFormateada, domicilio, dni, imagen);
-            usuarioService.loadUserByUsername(persona.getEmail());
-        }
-        else if (session.getAttribute("usuariosession") instanceof Usuario) {
-            Usuario usuario = (Usuario) session.getAttribute("usuariosession");
-            personaService.createPersona(nombre, apellido, sexo, dataFormateada, domicilio, dni, usuario, imagen);
-            usuarioService.loadUserByUsername(usuario.getEmail());
-        }
-
-        return "redirect:/";
     }
 
     @GetMapping("/imagen")
--- health-service-app/src/main/java/com/example/healthserviceapp/service/PacienteService.java
+++ health-service-app/src/main/java/com/example/healthserviceapp/service/PacienteService.java
@@ -1,84 +1,107 @@
 package com.example.healthserviceapp.service;
 
 import com.example.healthserviceapp.Exceptions.MiException;
+import com.example.healthserviceapp.entity.Imagen;
 import com.example.healthserviceapp.entity.Paciente;
-import com.example.healthserviceapp.entity.Persona;
+import com.example.healthserviceapp.entity.Usuario;
 import com.example.healthserviceapp.enums.ObraSocial;
+import com.example.healthserviceapp.enums.Sexo;
 import com.example.healthserviceapp.repository.PacienteRepository;
-import com.example.healthserviceapp.repository.PersonaRepository;
-import static java.lang.Boolean.FALSE;
+import com.example.healthserviceapp.repository.UsuarioRepository;
+import java.util.Date;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-/**
- *
- * @author ramir
- */
 @Service
 public class PacienteService {
 
     @Autowired
-    public PersonaRepository personaRepository;
+    public PacienteRepository pacienteRep;
 
     @Autowired
-    public PacienteRepository pacienteRepository;
+    private ImagenService imagenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRep;
 
     @Transactional
-    public void crearPaciente(ObraSocial obraSocial, Persona persona) throws MiException {
+    public void createPaciente(String nombre, String apellido,
+            Sexo sexo, Date fechaNacimiento, String domicilio, Integer dni,
+            Usuario usuario, MultipartFile archivo, ObraSocial obraSocial) throws MiException {
 
-        verificarObraSocial(obraSocial);
+        verificarNombre(nombre);
+        verificarApellido(apellido);
+        verificarDomicilio(domicilio);
+
         Paciente paciente = new Paciente();
-        
-            paciente.setId(persona.getId());
-            paciente.setEmail(persona.getEmail());
-            paciente.setPassword(persona.getPassword());
-            paciente.setRol(persona.getRol());
-            paciente.setActivo(persona.getActivo());
-            paciente.setNombre(persona.getNombre());
-            paciente.setApellido(persona.getApellido());
-            paciente.setDni(persona.getDni());
-            paciente.setDomicilio(persona.getDomicilio());
-            paciente.setFechaNacimiento(persona.getFechaNacimiento());
-            paciente.setSexo(persona.getSexo());
-            paciente.setImagen(null);
-            paciente.setObraSocial(obraSocial);
-            pacienteRepository.save(paciente);
-            eliminarPersona(persona);
+
+        paciente.setId(usuario.getId());
+        paciente.setEmail(usuario.getEmail());
+        paciente.setPassword(usuario.getPassword());
+        paciente.setRol(usuario.getRol());
+        paciente.setActivo(usuario.getActivo());
+        paciente.setNombre(nombre);
+        paciente.setApellido(apellido);
+        paciente.setDni(dni);
+        paciente.setDomicilio(domicilio);
+        paciente.setFechaNacimiento(fechaNacimiento);
+        paciente.setSexo(sexo);
+        paciente.setObraSocial(obraSocial);
+        Imagen imagen = imagenService.guardar(archivo);
+        paciente.setImagen(imagen);
+        pacienteRep.save(paciente);
+
+        borrarUsuario(usuario);
     }
 
     @Transactional
-    public void eliminarPersona(Persona persona){
-         personaRepository.delete(persona);
-    }
-    
-    @Transactional
-    public void modificarPaciente(ObraSocial obraSocial, String id) throws MiException {
-        verificarObraSocial(obraSocial);
-        Optional<Paciente> presente = pacienteRepository.findById(id);
-        Paciente paciente = new Paciente();
-        if (presente.isPresent()) {
-            paciente = presente.get();
-            paciente.setObraSocial(obraSocial);
-            pacienteRepository.save(paciente);
-        }
+    public void borrarUsuario(Usuario usuario) {
+        usuarioRep.delete(usuario);
     }
 
     @Transactional
-    public void eliminarPaciente(String id) {
+    public void modifyPaciente(Paciente paciente, String nombre, String apellido,
+            Sexo sexo, Date fechaNacimiento, String domicilio, Integer dni,
+            MultipartFile archivo, ObraSocial obraSocial) throws MiException {
 
-        Optional<Paciente> presente = pacienteRepository.findById(id);
-        if (presente.isPresent()) {
-            Paciente paciente = presente.get();
-            paciente.setActivo(FALSE);
-            pacienteRepository.save(paciente);
+        Optional<Paciente> respuesta = pacienteRep.findById(paciente.getId());
+
+        if (respuesta.isPresent()) {
+            Paciente nuevo_paciente = respuesta.get();
+            nuevo_paciente.setNombre(nombre);
+            nuevo_paciente.setApellido(apellido);
+            nuevo_paciente.setSexo(sexo);
+            nuevo_paciente.setDomicilio(domicilio);
+            nuevo_paciente.setFechaNacimiento(fechaNacimiento);
+            nuevo_paciente.setDni(dni);
+            nuevo_paciente.setObraSocial(obraSocial);
+            String id_imagen = nuevo_paciente.getImagen().getId();
+            Imagen imagen = imagenService.actualizar(id_imagen, archivo);
+            nuevo_paciente.setImagen(imagen);
+            pacienteRep.save(nuevo_paciente);
         }
     }
 
-    private void verificarObraSocial(ObraSocial obraSocial) throws MiException {
-        if (obraSocial == null) {
-            throw new MiException("La obra social no puede estar vacío");
+    public void verificarNombre(String nombre) throws MiException {
+        if (nombre == null || nombre.isEmpty() || nombre.trim().isEmpty()) {
+            throw new MiException("Error, el nombre no puede estar vacio");
+        }
+
+    }
+
+    public void verificarApellido(String apellido) throws MiException {
+        if (apellido == null || apellido.isEmpty() || apellido.trim().isEmpty()) {
+            throw new MiException("Error, el apellido no puede estar vacio");
         }
     }
+
+    public void verificarDomicilio(String domicilio) throws MiException {
+        if (domicilio == null || domicilio.isEmpty() || domicilio.trim().isEmpty()) {
+            throw new MiException("Error, el domicilio no puede estar vacio");
+        }
+    }
+
 }

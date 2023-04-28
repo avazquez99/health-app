1b5701309af9fc31c51972e4a4d729710db239bd
RamiroVillagra <ramirovillagra2@gmail.com>
2023-04-28 09:54:06 -0300

    Query de busqueda de profesional por localidad y especialidad. Agregue en el servicioPro los metodos para utilizar las querys

--- health-service-app/src/main/java/com/example/healthserviceapp/repository/ProfesionalRepository.java
+++ health-service-app/src/main/java/com/example/healthserviceapp/repository/ProfesionalRepository.java
@@ -4,8 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.healthserviceapp.entity.Profesional;
+import com.example.healthserviceapp.enums.Especialidad;
+import com.example.healthserviceapp.enums.Provincias;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ProfesionalRepository extends JpaRepository<Profesional, String>{
+    
+    @Query("SELECT p FROM Profesional p WHERE p.especialidad = :especialidad")
+    public List<Profesional> buscarPorEspecialidad(@Param("especialidad") Especialidad especialidad);
+    
+    @Query("SELECT p FROM Profesional p WHERE p.provincia = :provincia")
+    public List<Profesional> buscarPorLocalidad(@Param("provincia") Provincias provincia);
     
 }
